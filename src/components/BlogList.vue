c09abb8772c1e208aee41fac4edcbb8736ee669a
Shivendra Singh Tanwar <sunnysinghtanwar2@gmail.com>
2021-05-27 01:46:19 +0530

    Add blog list section with fake cards

--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -1,8 +1,241 @@
 <template>
   <section class="section has-background-white-bis">
     <div class="columns">
-      <div class="column">
-        Blog Listasdasdd
+      <div class="column is-offset-2 is-8">
+        <h1 class="title has-text-primary has-text-centered ">
+          Blog List
+        </h1>
+
+        <div class="columns">
+          <div class="column">
+            <div class="card is-horizontal">
+              <div class="card-image">
+                <figure class="image is-square is-256x256 ">
+                  <img src="https://images7.alphacoders.com/303/303995.jpg" alt="Placeholder image">
+                </figure>
+
+              </div>
+              <div class="card-stacked">
+                <div class="card-content">
+
+
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.<a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br>
+
+                  </div>
+
+                  <div class="media-content mt3">
+                    <div class="columns has-text-right">
+                      <div class="column has-text-right">
+                        <p class="title is-4 has-text-right">Author : </p>
+                      </div>
+                      <div class="column is-2 ">
+                        <p class="title is-5 ">John Smith</p>
+                        <p class="subtitle is-6">@johnsmith</p>
+
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+                <footer class="card-footer">
+                  <div class="card-footer-item has-text-right">
+                    <p class="subtitle is-6"><time datetime="2021-1-1">1 Jan 2021</time> </p>
+                  </div>
+                </footer>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <div class="card is-horizontal">
+              <div class="card-image">
+                <figure class="image is-square is-256x256 ">
+                  <img src="https://images7.alphacoders.com/303/303995.jpg" alt="Placeholder image">
+                </figure>
+
+              </div>
+              <div class="card-stacked">
+                <div class="card-content">
+
+
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.<a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br>
+
+                  </div>
+
+                  <div class="media-content mt3">
+                    <div class="columns has-text-right">
+                      <div class="column has-text-right">
+                        <p class="title is-4 has-text-right">Author : </p>
+                      </div>
+                      <div class="column is-2 ">
+                        <p class="title is-5 ">John Smith</p>
+                        <p class="subtitle is-6">@johnsmith</p>
+
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+                <footer class="card-footer">
+                  <div class="card-footer-item has-text-right">
+                    <p class="subtitle is-6"><time datetime="2021-1-1">1 Jan 2021</time> </p>
+                  </div>
+                </footer>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <div class="card is-horizontal">
+              <div class="card-image">
+                <figure class="image is-square is-256x256 ">
+                  <img src="https://images7.alphacoders.com/303/303995.jpg" alt="Placeholder image">
+                </figure>
+
+              </div>
+              <div class="card-stacked">
+                <div class="card-content">
+
+
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.<a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br>
+
+                  </div>
+
+                  <div class="media-content mt3">
+                    <div class="columns has-text-right">
+                      <div class="column has-text-right">
+                        <p class="title is-4 has-text-right">Author : </p>
+                      </div>
+                      <div class="column is-2 ">
+                        <p class="title is-5 ">John Smith</p>
+                        <p class="subtitle is-6">@johnsmith</p>
+
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+                <footer class="card-footer">
+                  <div class="card-footer-item has-text-right">
+                    <p class="subtitle is-6"><time datetime="2021-1-1">1 Jan 2021</time> </p>
+                  </div>
+                </footer>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <div class="card is-horizontal">
+              <div class="card-image">
+                <figure class="image is-square is-256x256 ">
+                  <img src="https://images7.alphacoders.com/303/303995.jpg" alt="Placeholder image">
+                </figure>
+
+              </div>
+              <div class="card-stacked">
+                <div class="card-content">
+
+
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.<a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br>
+
+                  </div>
+
+                  <div class="media-content mt3">
+                    <div class="columns has-text-right">
+                      <div class="column has-text-right">
+                        <p class="title is-4 has-text-right">Author : </p>
+                      </div>
+                      <div class="column is-2 ">
+                        <p class="title is-5 ">John Smith</p>
+                        <p class="subtitle is-6">@johnsmith</p>
+
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+                <footer class="card-footer">
+                  <div class="card-footer-item has-text-right">
+                    <p class="subtitle is-6"><time datetime="2021-1-1">1 Jan 2021</time> </p>
+                  </div>
+                </footer>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <div class="card is-horizontal">
+              <div class="card-image">
+                <figure class="image is-square is-256x256 ">
+                  <img src="https://images7.alphacoders.com/303/303995.jpg" alt="Placeholder image">
+                </figure>
+
+              </div>
+              <div class="card-stacked">
+                <div class="card-content">
+
+
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.<a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br>
+
+                  </div>
+
+                  <div class="media-content mt3">
+                    <div class="columns has-text-right">
+                      <div class="column has-text-right">
+                        <p class="title is-4 has-text-right">Author : </p>
+                      </div>
+                      <div class="column is-2 ">
+                        <p class="title is-5 ">John Smith</p>
+                        <p class="subtitle is-6">@johnsmith</p>
+
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+                <footer class="card-footer">
+                  <div class="card-footer-item has-text-right">
+                    <p class="subtitle is-6"><time datetime="2021-1-1">1 Jan 2021</time> </p>
+                  </div>
+                </footer>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -10,10 +243,72 @@
 
 <script>
 export default {
-name: "BlogList"
+  name: "BlogList"
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.card {
+  &.is-horizontal {
+    display: flex;
 
+    .card-image {
+      //width: 100%;
+      //height: 100%;
+    }
+
+    .card-stacked {
+      flex-direction: column;
+      flex: 1 1 auto;
+      display: flex;
+      position: relative;
+
+      .card-content {
+        flex-grow: 1;
+      }
+    }
+  }
+
+  &.is-fullimage {
+    background-color: transparent;
+    .card-image {
+      color: #fff !important;
+      .card-stacked {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        background: linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0.65) 100%);
+        text-shadow: 0px 0px 2px #000000;
+
+        .card-footer {
+          border-color: transparent;
+        }
+
+        .title,
+        .subtitle {
+          color: inherit;
+        }
+
+        a {
+          color: inherit;
+
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+      }
+    }
+  }
+
+  //   .card-footer {
+  //     .card-footer-item {
+  //       padding-left: 1.5rem;
+  //       padding-right: 1.5rem;
+  //     }
+
+  //     div.card-footer-item {
+  //       justify-content: unset;
+  //     }
+  //   }
+}
 </style>
