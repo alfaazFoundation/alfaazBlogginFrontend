3f24f364ee532e6bba07f14f34393b101685bc7e
divyani1997 <divyanidevadiya@gmail.com>
2021-06-01 11:10:31 +0530

    vue editor

--- src/views/dashboard/CreateNewBlog.vue
+++ src/views/dashboard/CreateNewBlog.vue
@@ -8,11 +8,8 @@
     <div class="columns ">
       <div class="column">
         <b-field >
-          <b-input
-              placeholder="Message"
-              type="textarea"
-          >
-          </b-input>
+        
+         <Vueditor></Vueditor>
         </b-field>
       </div>
     </div>
@@ -120,6 +117,30 @@
 
 <script>
 // @ is an alias to /src
+//import MediumEditor from 'vuejs-medium-editor';
+import Vue from 'vue';
+import Vuex from 'vuex'
+import Vueditor from 'vueditor'
+import 'vueditor/dist/style/vueditor.min.css' 
+
+let config = {
+  toolbar: [
+    'removeFormat', 'undo', '|', 'elements', 'fontName', 'fontSize', 'foreColor', 'backColor', 'divider',
+    'bold', 'italic', 'underline', 'strikeThrough', 'links', 'divider', 'subscript', 'superscript',
+    'divider', 'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', '|', 'indent', 'outdent',
+    'insertOrderedList', 'insertUnorderedList', '|', 'picture', 'tables', '|', 'switchView'
+  ],
+  fontName: [
+    {val: 'arial black'}, 
+    {val: 'times new roman'}, 
+    {val: 'Courier New'}
+  ],
+  fontSize: ['12px', '14px', '16px', '18px', '0.8rem', '1.0rem', '1.2rem', '1.5rem', '2.0rem'],
+  uploadUrl: ''
+};
+
+Vue.use(Vuex);
+Vue.use(Vueditor, config);
 
 export default {
   name: 'CreateNewBlog',
