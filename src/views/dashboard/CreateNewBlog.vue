c8081de67a700ef39045536f231e83e636d76c59
Shivendra Singh Tanwar <sunnysinghtanwar2@gmail.com>
2021-06-01 20:35:38 +0530

    updated ui for create new blog

--- src/views/dashboard/CreateNewBlog.vue
+++ src/views/dashboard/CreateNewBlog.vue
@@ -1,39 +1,43 @@
 <template>
   <section class="section has-background-white-ter same-page-height ">
-    <div class="columns ">
-      <div class="column">
-        <h1 class="title has-text-primary">This is new blog</h1>
-      </div>
-    </div>
-    <div class="columns ">
-      <div class="column">
-        <b-field label="Start with an eye-catching title!">
-        <b-input placeholder="Title">
-          </b-input>
-        </b-field>
-      </div>
-    </div>
-    <div class="columns ">
-      <div class="column">
-        <b-field label ="This canvas is all yours!">
-          <Vueditor class="same-page-height has-side-shadow"></Vueditor>
-        </b-field>
-      </div>
-    </div>
-    <div class="columns ">
-      <div class="column">
-        <div class="field is-grouped is-grouped-centered">
+    <div class="columns">
+      <div class="column box">
+        <div class="columns ">
+          <div class="column">
+            <h1 class="title has-text-primary">Create something magical!</h1>
+          </div>
+        </div>
+        <div class="columns ">
+          <div class="column">
+            <b-field label="Start with an eye-catching title!">
+              <b-input placeholder="Title">
+              </b-input>
+            </b-field>
+          </div>
+        </div>
+        <div class="columns ">
+          <div class="column">
+            <b-field label ="This canvas is all yours!">
+              <Vueditor class="" style="min-height: 50rem; border: solid thin black"></Vueditor>
+            </b-field>
+          </div>
+        </div>
+        <div class="columns ">
+          <div class="column">
+            <div class="field is-grouped is-grouped-centered">
               <p class="control">
                 <button class="button is-primary">
                   Save As Draft
                 </button>
               </p>
-               <p class="control">
+              <p class="control">
                 <button class="button is-primary">
                   Publish
                 </button>
               </p>
             </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
