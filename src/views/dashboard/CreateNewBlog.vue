75332f8c3d68e7f9dc6a88bf4795a933e6a86ad2
divyani1997 <divyanidevadiya@gmail.com>
2021-06-01 16:45:03 +0530

    create new blog editor

--- src/views/dashboard/CreateNewBlog.vue
+++ src/views/dashboard/CreateNewBlog.vue
@@ -7,109 +7,33 @@
     </div>
     <div class="columns ">
       <div class="column">
-        <b-field >
-        
-         <Vueditor></Vueditor>
-        </b-field>
-      </div>
-    </div>
-    <div class="columns ">
-      <div class="column">
-        <b-field >
-          <b-input
-              placeholder="Message"
-              type="textarea"
-          >
+        <b-field label="Start with an eye-catching title!">
+        <b-input placeholder="Title">
           </b-input>
         </b-field>
       </div>
     </div>
     <div class="columns ">
       <div class="column">
-        <b-field >
-          <b-input
-              placeholder="Message"
-              type="textarea"
-          >
-          </b-input>
+        <b-field label ="This canvas is all yours!">
+          <Vueditor class="same-page-height has-side-shadow"></Vueditor>
         </b-field>
       </div>
     </div>
     <div class="columns ">
       <div class="column">
-        <b-field >
-          <b-input
-              placeholder="Message"
-              type="textarea"
-          >
-          </b-input>
-        </b-field>
-      </div>
-    </div>
-    <div class="columns ">
-      <div class="column">
-        <b-field >
-          <b-input
-              placeholder="Message"
-              type="textarea"
-          >
-          </b-input>
-        </b-field>
-      </div>
-    </div>
-    <div class="columns ">
-      <div class="column">
-        <b-field >
-          <b-input
-              placeholder="Message"
-              type="textarea"
-          >
-          </b-input>
-        </b-field>
-      </div>
-    </div>
-    <div class="columns ">
-      <div class="column">
-        <b-field >
-          <b-input
-              placeholder="Message"
-              type="textarea"
-          >
-          </b-input>
-        </b-field>
-      </div>
-    </div>
-    <div class="columns ">
-      <div class="column">
-        <b-field >
-          <b-input
-              placeholder="Message"
-              type="textarea"
-          >
-          </b-input>
-        </b-field>
-      </div>
-    </div>
-    <div class="columns ">
-      <div class="column">
-        <b-field >
-          <b-input
-              placeholder="Message"
-              type="textarea"
-          >
-          </b-input>
-        </b-field>
-      </div>
-    </div>
-    <div class="columns ">
-      <div class="column">
-        <b-field >
-          <b-input
-              placeholder="Message"
-              type="textarea"
-          >
-          </b-input>
-        </b-field>
+        <div class="field is-grouped is-grouped-centered">
+              <p class="control">
+                <button class="button is-primary">
+                  Save As Draft
+                </button>
+              </p>
+               <p class="control">
+                <button class="button is-primary">
+                  Publish
+                </button>
+              </p>
+            </div>
       </div>
     </div>
   </section>
@@ -128,7 +52,7 @@ let config = {
     'removeFormat', 'undo', '|', 'elements', 'fontName', 'fontSize', 'foreColor', 'backColor', 'divider',
     'bold', 'italic', 'underline', 'strikeThrough', 'links', 'divider', 'subscript', 'superscript',
     'divider', 'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', '|', 'indent', 'outdent',
-    'insertOrderedList', 'insertUnorderedList', '|', 'picture', 'tables', '|', 'switchView'
+    'insertOrderedList', 'insertUnorderedList', '|', 'picture', 'tables', '|', 'switchView' 
   ],
   fontName: [
     {val: 'arial black'}, 
