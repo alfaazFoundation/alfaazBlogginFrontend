f268fd2ff17df2ebc7c1ef69cddf8d4520cdbfd2
Shivendra Singh Tanwar <sunnysinghtanwar2@gmail.com>
2021-06-09 01:27:01 +0530

    Made custom blog card component for home page

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="card is-horizontal">
+    <div class="card-image">
+      <figure class="image is-square is-256x256 is-hidden-touch">
+        <img :src="image" alt="Image">
+      </figure>
+    </div>
+    <div class="card-stacked">
+      <div class="card-content pb0">
+        <div class="content mb0">
+          <div >
+            <p class="title is-3 mb2 blog-card-title">{{ title }}</p>
+
+            <p class="subtitle is-5 ">
+              <!--              <span>
+                              <small>
+                                {{ getHumanDateFromUnixTime(publishedOn) }}
+                              </small>
+                            </span>-->
+               - {{ authorName }}
+            </p>
+          </div>
+          <hr class="is-primary mt-1">
+          <div class="blog-card-content">
+            {{content}}
+          </div>
+          <div class="has-text-right mt2-5">
+            {{ getHumanDateFromUnixTime(publishedOn) }}
+          </div>
+        </div>
+        <nav class="level is-mobile">
+          <div class="level-left">
+            <a class="level-item" aria-label="reply">
+            <span class="icon is-small">
+              <i class="fas fa-reply" aria-hidden="true"></i>
+            </span>
+            </a>
+            <a class="level-item" aria-label="retweet">
+            <span class="icon is-small">
+              <i class="fas fa-retweet" aria-hidden="true"></i>
+            </span>
+            </a>
+            <a class="level-item" aria-label="like">
+            <span class="icon is-small">
+              <i class="fas fa-heart" aria-hidden="true"></i>
+            </span>
+            </a>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogCard",
+  props:{
+    image: {
+      type: String,
+      default: 'https://images7.alphacoders.com/303/303995.jpg'
+    },
+    title: String,
+    content: String,
+    authorName: String,
+    publishedOn: Number
+  },
+  methods:{
+    getHumanDateFromUnixTime(unixTimestamp){
+      let dateObject = new Date(unixTimestamp); // The 0 there is the key, which sets the date to the epoch
+      // dateObject.setUTCSeconds(unixTimestamp);
+/*
+      dateObject.toLocaleString("en-US", {weekday: "long"}) // Monday
+      dateObject.toLocaleString("en-US", {month: "long"}) // December
+      dateObject.toLocaleString("en-US", {day: "numeric"}) // 9
+      dateObject.toLocaleString("en-US", {year: "numeric"}) // 2019
+      dateObject.toLocaleString("en-US", {hour: "numeric"}) // 10 AM
+      dateObject.toLocaleString("en-US", {minute: "numeric"}) // 30*/
+      let localeDateString = dateObject.toLocaleString("en-US", {month: "long",day: "numeric",year:'numeric'}) // 15
+      // dateObject.toLocaleString("en-US", {timeZoneName: "short"})
+
+      // let localeDateString = dateObject.toLocaleDateString()
+
+      console.log('Converted local date string - ',localeDateString);
+
+      return localeDateString;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.blog-card-content{
+  width: 100%;
+  height: 3rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.blog-card-title{
+  width: 100%;
+  height: 2.2rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -8,181 +8,49 @@
 
         <div class="columns">
           <div class="column is-12-mobile ">
-            <div class="card is-horizontal">
-              <div class="card-image">
-                <figure class="image is-square is-256x256 is-hidden-touch">
-                  <img src="https://images7.alphacoders.com/303/303995.jpg" alt="Placeholder image">
-                </figure>
-<!--                <figure class="image is-square is-64x64 is-hidden-desktop">
-                  <img src="https://images7.alphacoders.com/303/303995.jpg" alt="Placeholder image">
-                </figure>-->
-              </div>
-              <div class="card-stacked">
-                <div class="card-content">
-                  <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.<a>@bulmaio</a>.
-                    <a href="#">#css</a> <a href="#">#responsive</a>
-
-                  </div>
-                  <div class="media-content mt3">
-                    <div class="columns has-text-right has-text-centered-touch">
-                      <div class="column  has-text-centered-touch">
-                        <p class="title is-4 ">Author : </p>
-                      </div>
-                      <div class="column is-2 ">
-                        <p class="title is-5 ">John Smith</p>
-                        <p class="subtitle is-6">@johnsmith</p>
-
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-                <footer class="card-footer">
-                  <div class="card-footer-item has-text-right has-text-centered-touch">
-                    <p class="subtitle is-6"><time datetime="2021-1-1">1 Jan 2021</time> </p>
-                  </div>
-                </footer>
-              </div>
-            </div>
+            <blog-card
+              :title="blog.title"
+              :content="blog.content"
+              :author-name="blog.authorName"
+              :published-on="blog.publishedOn"
+            >
+            </blog-card>
           </div>
         </div>
 
         <div class="columns">
           <div class="column is-12-mobile ">
-            <div class="card is-horizontal">
-              <div class="card-image">
-                <figure class="image is-square is-256x256 is-hidden-touch">
-                  <img src="https://images7.alphacoders.com/303/303995.jpg" alt="Placeholder image">
-                </figure>
-                <!--                <figure class="image is-square is-64x64 is-hidden-desktop">
-                                  <img src="https://images7.alphacoders.com/303/303995.jpg" alt="Placeholder image">
-                                </figure>-->
-              </div>
-              <div class="card-stacked">
-                <div class="card-content">
-                  <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.<a>@bulmaio</a>.
-                    <a href="#">#css</a> <a href="#">#responsive</a>
-
-                  </div>
-                  <div class="media-content mt3">
-                    <div class="columns has-text-right has-text-centered-touch">
-                      <div class="column  has-text-centered-touch">
-                        <p class="title is-4 ">Author : </p>
-                      </div>
-                      <div class="column is-2 ">
-                        <p class="title is-5 ">John Smith</p>
-                        <p class="subtitle is-6">@johnsmith</p>
-
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-                <footer class="card-footer">
-                  <div class="card-footer-item has-text-right has-text-centered-touch">
-                    <p class="subtitle is-6"><time datetime="2021-1-1">1 Jan 2021</time> </p>
-                  </div>
-                </footer>
-              </div>
-            </div>
+            <blog-card
+                :title="blog.title"
+                :content="blog.content"
+                :author-name="blog.authorName"
+                :published-on="blog.publishedOn"
+            >
+            </blog-card>
           </div>
         </div>
 
         <div class="columns">
           <div class="column is-12-mobile ">
-            <div class="card is-horizontal">
-              <div class="card-image">
-                <figure class="image is-square is-256x256 is-hidden-touch">
-                  <img src="https://images7.alphacoders.com/303/303995.jpg" alt="Placeholder image">
-                </figure>
-                <!--                <figure class="image is-square is-64x64 is-hidden-desktop">
-                                  <img src="https://images7.alphacoders.com/303/303995.jpg" alt="Placeholder image">
-                                </figure>-->
-              </div>
-              <div class="card-stacked">
-                <div class="card-content">
-                  <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.<a>@bulmaio</a>.
-                    <a href="#">#css</a> <a href="#">#responsive</a>
-
-                  </div>
-                  <div class="media-content mt3">
-                    <div class="columns has-text-right has-text-centered-touch">
-                      <div class="column  has-text-centered-touch">
-                        <p class="title is-4 ">Author : </p>
-                      </div>
-                      <div class="column is-2 ">
-                        <p class="title is-5 ">John Smith</p>
-                        <p class="subtitle is-6">@johnsmith</p>
-
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-                <footer class="card-footer">
-                  <div class="card-footer-item has-text-right has-text-centered-touch">
-                    <p class="subtitle is-6"><time datetime="2021-1-1">1 Jan 2021</time> </p>
-                  </div>
-                </footer>
-              </div>
-            </div>
+            <blog-card
+                :title="blog.title"
+                :content="blog.content"
+                :author-name="blog.authorName"
+                :published-on="blog.publishedOn"
+            >
+            </blog-card>
           </div>
         </div>
 
         <div class="columns">
           <div class="column is-12-mobile ">
-            <div class="card is-horizontal">
-              <div class="card-image">
-                <figure class="image is-square is-256x256 is-hidden-touch">
-                  <img src="https://images7.alphacoders.com/303/303995.jpg" alt="Placeholder image">
-                </figure>
-                <!--                <figure class="image is-square is-64x64 is-hidden-desktop">
-                                  <img src="https://images7.alphacoders.com/303/303995.jpg" alt="Placeholder image">
-                                </figure>-->
-              </div>
-              <div class="card-stacked">
-                <div class="card-content">
-                  <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.<a>@bulmaio</a>.
-                    <a href="#">#css</a> <a href="#">#responsive</a>
-
-                  </div>
-                  <div class="media-content mt3">
-                    <div class="columns has-text-right has-text-centered-touch">
-                      <div class="column  has-text-centered-touch">
-                        <p class="title is-4 ">Author : </p>
-                      </div>
-                      <div class="column is-2 ">
-                        <p class="title is-5 ">John Smith</p>
-                        <p class="subtitle is-6">@johnsmith</p>
-
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-                <footer class="card-footer">
-                  <div class="card-footer-item has-text-right has-text-centered-touch">
-                    <p class="subtitle is-6"><time datetime="2021-1-1">1 Jan 2021</time> </p>
-                  </div>
-                </footer>
-              </div>
-            </div>
+            <blog-card
+                :title="blog.title"
+                :content="blog.content"
+                :author-name="blog.authorName"
+                :published-on="blog.publishedOn"
+            >
+            </blog-card>
           </div>
         </div>
       </div>
@@ -191,8 +59,20 @@
 </template>
 
 <script>
+import BlogCard from "@/components/BlogCard";
 export default {
-  name: "BlogList"
+  name: "BlogList",
+  components: {BlogCard},
+  data() {
+    return {
+      blog:{
+        title:"First Title that can be long you kn be long you kn be long you kn be long you kn be long you knowcan be long you knowcan be long you know ",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis., consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis., con",
+        authorName:"Shivendra Singh Tanwar",
+        publishedOn:1623175822307
+      }
+    }
+  }
 }
 </script>
 
